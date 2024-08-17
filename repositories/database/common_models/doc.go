8d488d15253b4b87fa3021b7a80a381985206f18
rhodeon <rhodeondev@gmail.com>
2024-08-17 05:23:47 +0100

    Added package doc.

---
+++ repositories/database/common_models/doc.go
@@ -0,0 +1,4 @@
+// Package common_models provides unified forms of structs gotten from the sqlc repository.
+// Due to the models being in separate directories, they can't be used interchangeably, hence the need of resolved models
+// for identical output.
+package common_models
